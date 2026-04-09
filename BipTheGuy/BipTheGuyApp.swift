cbe91d96c1ff3477aa3a4ae896a969f84ac0833d
gabrielabizarro <gbizarro2027@gmail.com>
2026-04-09 11:35:49 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by BIZARRO, GABRIELA on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
