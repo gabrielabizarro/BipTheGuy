85da4fe28be104ede858dd350f3f329693095769
gabrielabizarro <gbizarro2027@gmail.com>
2026-04-13 13:27:39 -0700

    04/13/26

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -7,50 +7,67 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
+        @State private var isFullSize = true
+        @State private var selectedPhoto: PhotosPickerItem?
+        @State private var bipImage = Image("clown")
     
     var body: some View {
         VStack {
             Spacer()
             
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
+                .scaleEffect(isFullSize ? 1.0 : 0.9)
                 .onTapGesture {
-                    playSounds(soundName: "punchSound")
+                    playSound(soundName: "punchSound")
+                    isFullSize = false // wil immediately shrink using .scaleEffect to 90% of soze
+                    withAnimation (.spring(response: 0.3, dampingFraction: 0.3)) {
+                        isFullSize = true // will go from 90% to 100% size but using the .spring animation
+                    }
                 }
             
             Spacer()
             
-            Button {
-                //TODO: button action here
-            } label: {
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
             }
-
-        }
-        .padding()
-    }
-    
-    func playSounds(soundName: String){
-        if audioPlayer != nil && audioPlayer.isPlaying {
-            audioPlayer.stop()
+            .onChange(of: selectedPhoto) {
+                Task {
+                    guard let selectedImage = try? await selectedPhoto?.loadTransferable(type: Image.self) else {
+                        print("😡 ERROR: Could not get Image from loadTransferrable")
+                        return
+                    }
+                    bipImage = selectedImage
+                }
+                
             }
-        guard let soundFile = NSDataAsset(name: soundName) else {
-            print ("😡 Could not read file named \(soundName)")
-            return
         }
-        do {
-            audioPlayer = try AVAudioPlayer(data: soundFile.data)
-            audioPlayer.play()
-        } catch {
-            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+            .padding()
         }
-    }
-}
+        
+    func playSound(soundName: String) {
+           if audioPlayer != nil && audioPlayer.isPlaying {
+                       audioPlayer.stop()
+           }
+           guard let soundFile = NSDataAsset(name: soundName) else {
+               print("😡 Could not read file named \(soundName)")
+               return
+           }
+           do {
+               audioPlayer = try AVAudioPlayer(data: soundFile.data)
+               audioPlayer.play()
+           } catch {
+               print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+           }
+       }
+   }
 
-#Preview {
-    ContentView()
-}
+    
+    #Preview {
+        ContentView()
+    }
